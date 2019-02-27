a35686ca52b2d5b94ff85cd79118298a6846da35
Travis Martin <travisemerymartin@gmail.com>
2019-02-27 22:00:43 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(entry)
+  response = ''
+  if entry % 3 == 0
+    response = response + 'Fizz'
+  end
+  if entry % 5 == 0
+    response = response + 'Buzz'
+  elsif response == ''
+    return nil
+  end
+  print response
+  return response
+end
